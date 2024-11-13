1dc42f226842331152f183cf2656c9b000cffc0e
rafly-dev <raflydevelop@gmail.com>
2024-11-13 09:31:12 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashScr(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ lib/screens/splash_scr.dart
@@ -0,0 +1,14 @@
+class SplashScr extends StatefulWidget {
+  const SplashScr({super.key});
+
+  @override
+  State<SplashScr> createState() => _SplashScrState();
+}
+
+class _SplashScrState extends State<SplashScr> {
+  @override
+  Widget build(BuildContext context) {
+    // return const Placeholder();
+    return Scaffold();
+  }
+}
